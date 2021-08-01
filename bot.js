739ff14034209b61f0ab2f3829d240328bed3b06
Hüsnü Əhədov <80684361+sirincay@users.noreply.github.com>
2021-08-02 01:14:59 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
 const { Telegraf } = require('telegraf')
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf('1842730159:AAFr_LAxQCS445V2LW18oTRJeXjiPIk2uug')
 
 
-bot.start((ctx) => ctx.reply(`Salam ${ctx.from.username}. mənə gizli və ya açıq etiraf edirsən məndə onu öz kanalımda paylaşıram. Etirafını yazdıqdan sonra Gizli və ya Açıq paylaşacağım barədə soruşacam. Buyur etirafını et 🤎`))
+bot.start((ctx) => ctx.reply(`Salam ${ctx.from.id}. mənə gizli və ya açıq etiraf edirsən məndə onu öz kanalımda paylaşıram. Etirafını yazdıqdan sonra Gizli və ya Açıq paylaşacağım barədə soruşacam. Buyur etirafını et 🤎`))
 
 
 
