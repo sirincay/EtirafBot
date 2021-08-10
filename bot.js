693d9ecc1ba81eb01db052f3e246db1eb21c21b9
Hüsnü Əhədov <80684361+sirincay@users.noreply.github.com>
2021-08-10 17:26:02 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,18 +3,21 @@ const { Telegraf } = require('telegraf')
 const bot = new Telegraf('1925679701:AAErqvG0NeLMWAJa1QhNnExI6fBmDcE2EmA')
 
 
-bot.start((ctx) => ctx.reply(`Salam ${ctx.from.username}. mənə gizli və ya açıq etraf edirsən məndə onu öz kanalımda paylaşıram. Etirafını yazdıqdan sonra Gizli və ya Açıq paylaşacağım barədə soruşacam. Buyur etirafını et 🤎`))
+bot.start((ctx) => ctx.reply(Salam ${ctx.from.first_name}. mənə gizli və ya açıq etiraf edirsən məndə onu öz kanalımda paylaşıram. Etirafını yazdıqdan sonra Gizli və ya Açıq paylaşacağım barədə soruşacam. Buyur etirafını et 🤎))
 
 
 
 bot.command("admin", ctx => {
-	if(ctx.from.id===1799203251){
-		ctx.reply("Giriş uğurludur.")
-	}else{
-		ctx.reply("Giriş uğursuzdur.")
-	
-	}
-	
+  if(ctx.from.id===CREATOR_ID){
+    ctx.reply("Giriş uğurludur.")
+  }else{
+    ctx.reply("Giriş uğursuzdur.")
+  
+  }
+})
+
+
+
 let metn;
 
 bot.on("text", ctx => {
@@ -30,29 +33,35 @@ ctx.telegram.sendMessage(ctx.from.id, 'Etirafınız necə paylaşılsın?',{
     ]
   }
 })
+  
+
 })
 
 bot.action("Açıq☠️ ", ctx => {
-	let aciqetiraf= `Etiraf edən şəxs ☠️ : ${ctx.from.id}\n\n\n`
-	let kanalid = -1001424334391
-	var seliqe = `Etdiyi etiraf ✍️ : ${metn}\n\n\n`
-	var sonda = 'Etiraf etmək üçün ☂️ : @ConfessTrBot-a yazın.'
-	ctx.telegram.sendMessage(kanalid, `${aciqetiraf+seliqe+sonda}`)
-	ctx.reply('Etirafınız göndərildi. Yoxlanıldıqdan sonra @ConfessTr kanalında paylaşılacaq.')
+  let aciqetiraf= Etiraf edən şəxs ☠️ : ${ctx.from.first_name}\n\n\n
+  let kanalid = -1001580366831
+  var seliqe = Etdiyi etiraf ✍️ : ${metn}\n\n\n
+  var sonda = 'Etiraf etmək üçün ☂️ : @MyEtirafBot-a yazın.'
+  ctx.telegram.sendMessage(kanalid, ${aciqetiraf+seliqe+sonda})
+  ctx.reply('Etirafınız göndərildi. Yoxlanıldıqdan sonra @MenimEtirafim kanalında paylaşılacaq.')
 })
 
 
 
 bot.action("Gizli☠️ ", ctx => {
-	let eden = ctx.from.first_name 
-	let aciqetiraf= `Etiraf edən şəxs ☠️ : Anonim\n\n\n`
-	let kanalid = -1001424334391
-	var seliqe = `Etdiyi etiraf ✍️ : ${metn}\n\n\n`
-	var sonda = 'Etiraf etmək üçün ☂️ : @ConfessTrBot -a yazın.'
-	ctx.telegram.sendMessage(kanalid, `${eden}\n\n\n${aciqetiraf+seliqe+sonda}`)
-	ctx.reply('Etirafınız göndərildi. Yoxlanıldıqdan sonra @ConfessTr kanalında paylaşılacaq.')
+  let eden = ctx.from.first_name 
+  let aciqetiraf= Etiraf edən şəxs ☠️ : Anonim\n\n\n
+  let kanalid = -1001525895163
+  var seliqe = Etdiyi etiraf ✍️ : ${metn}\n\n\n
+  var sonda = 'Etiraf etmək üçün ☂️ : @MyEtirafBot-a yazın.'
+  ctx.telegram.sendMessage(kanalid, ${eden}\n\n\n${aciqetiraf+seliqe+sonda})
+  ctx.reply('Etirafınız göndərildi. Yoxlanıldıqdan sonra @MenimEtirafim kanalında paylaşılacaq.')
 })
 
-console.log('Zro husnu')
+
+
+
+
+console.log('Zro Ferid')
 
 bot.launch()
