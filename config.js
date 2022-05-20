005069843710258a50ae525b6dcb74202dacdc6c
Hüsnü Əhədov <80684361+sirincay@users.noreply.github.com>
2022-05-20 22:11:40 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,13 @@
 module.exports = ({
 
-	token: "TOKEN yaz",
-    
+token: "TOKEN yaz",
+startmesaj: "<b>Salam. Bota etiraf etmək istədiyin mesajı yazırsan və kanalda paylaşılır.\n\nEtirafınızı yazdıxdan sonra Anonim(gizli) yoxsa Açıx paylaşım soruşacam ✅\n\nBuyur Bir Etiraf Et 💁</b>🏼‍♀️",
+kanalid: "ETİRAF KANAL ID YAZ",
+aciqetirafeden: "Etiraf Edən:",
+aciqyazdigietiraf: "Yazdığı etiraf: ",
+sonluq: "Sizde etiraf etmek isteyirsinizse @botumuza yazin",
+anonimetirafeden: "Etiraf Edən: Anonim",
+anonimyazdigietiraf: "Yazdığı etiraf: ",
+etirafkanal: "@EtirafKanalin"
+	
 });
